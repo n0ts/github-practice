a40712032b7a2624d999ea8c5f75a2ef9ca2dcfb
Naoya Nakazawa <naoya.n@gmail.com>
2016-04-23 08:54:44 +0900

    Auto detect git config

--- git-gpg.sh
+++ git-gpg.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
-    echo "Usage $(basename $0) <email>"
-    exit 1
+email=$1
+if [ -z "$email" ]; then
+    email=$(git config user.email)
 fi
 
 gpg=gpg
@@ -10,7 +10,7 @@ if [ $(which gpg2) ]; then
     gpg=gpg2
 fi
 
-gpg_pub_key=$($gpg --list-keys | grep -1 "<$1>"| grep -e "^pub")
+gpg_pub_key=$($gpg --list-keys | grep -1 "<$email>"| grep -e "^pub")
 gpg_pub_key_id=$(echo $gpg_pub_key | cut -d ' ' -f 2 | cut -d '/' -f 2)
 if [ -z "$gpg_pub_key_id" ]; then
     echo "Could not get gpg pub key - $gpg_pub_key_id"
